de5df69c5d8f3d056e043861a38633ccdbe6cf6f
mannuelst <manuelds@outlook.pt>
2024-09-16 02:05:42 +0100

    feat: create user route

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,35 @@
-import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
-export const prisma = new PrismaClient()
+import { z } from "zod";
+import { prisma } from "./lib/prisma";
 
 
-prisma.user.create({
-  data: {
-    nome: "Manuel",
-    email: "manuel@email.com"
-  }
-})
+
+// prisma.user.create({
+//   data: {
+//     nome: "Manuel",
+//     email: "manuel@email.com"
+//   }
+// })
 
 export const app = fastify()
+
+
+app.post("/users", async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6)
+  })
+
+  const { email, name, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password
+    }
+  })
+
+  return reply.status(201).send()
+})
