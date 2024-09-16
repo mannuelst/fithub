e54acee01ead1b59fb100be42e9cef7f13ab8a19
mannuelst <manuelds@outlook.pt>
2024-09-16 01:20:24 +0100

    add prisma client

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+export const prisma = new PrismaClient()
 
 
+prisma.user.create({
+  data: {
+    nome: "Manuel",
+    email: "manuel@email.com"
+  }
+})
 
 export const app = fastify()
