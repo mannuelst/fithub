6ba459bf410aa40359b7f72463ece28050b651bb
mannuelst <manuelds@outlook.pt>
2024-09-18 03:32:02 +0100

    feat: set global handle error

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,7 @@
 import { PrismaUserRepository } from '@/repositories/prisma/prisma-users-repository';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
-import { UserAlreadyExistsError } from '../use-cases/errors/user-alread-exists-error';
+import { UserAlreadyExistsError } from '../use-cases/errors/user-already-exists-error';
 import { RegisterUseCase } from '../use-cases/register';
 
 
@@ -22,7 +22,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     if (error instanceof UserAlreadyExistsError) {
       return reply.status(409).send({ message: error.message })
     }
-    return reply.status(500).send()
+    throw error
 
   }
 
--- src/http/use-cases/register.ts
+++ src/http/use-cases/register.ts
@@ -1,6 +1,6 @@
 import { UserRepository } from "@/repositories/user-repository";
 import { hash } from "bcrypt";
-import { UserAlreadyExistsError } from "./errors/user-alread-exists-error";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 
 
 interface RegisterUseCaseRequest {
